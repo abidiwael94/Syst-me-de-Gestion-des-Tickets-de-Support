09b584fed9865f9df913e50f6202ea76176f1bf1
AnIscoder01 <anisrojo519@gmail.com>
2025-04-17 20:21:57 +0100

    implementation Auth user

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => console.log('MongoDB connecté'))
+    .catch(err => console.log(err));
+
+
+app.use('/api/auth', require('./routes/authRoutes'));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
+    
