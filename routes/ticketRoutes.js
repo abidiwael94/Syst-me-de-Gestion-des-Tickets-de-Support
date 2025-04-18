d07b354ac07e87ad34aefff56c689c1d1e1c1ba3
mohamedkhadhri <mohamed.24292770@gmail.com>
2025-04-18 12:58:48 +0100

    ajouter action delete pour les Tikets

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -22,4 +22,18 @@ router.put('/:id', auth(['admin']), async (req, res) => {
   res.json(ticket);
 });
 
+
+router.delete('/:id', auth(['admin']), async (req, res) => {
+  try {
+    const deletedTicket = await Ticket.findByIdAndDelete(req.params.id);
+    if (!deletedTicket) {
+      return res.status(404).json({ message: 'Ticket non trouvé' });
+    }
+    res.json({ message: 'Ticket supprimé avec succès' });
+  } catch (error) {
+    res.status(500).json({ message: 'Erreur lors de la suppression du ticket', error });
+  }
+});
+
+
 module.exports = router;
