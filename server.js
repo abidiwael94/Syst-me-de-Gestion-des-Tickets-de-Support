76299a41ee3d11bd82e391372bd704b6b55c346f
abidiwael94 <aw.abidi.wael@gmail.com>
2025-04-21 20:58:37 +0100

    setup node mailer + extract db connection to new file db.js

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
-const express = require("express")
-const mongoose = require("mongoose")
-
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
+
+const connectDB = require('./config/db'); // <--- import DB
 const app = express();
+
+connectDB(); // <--- connect to MongoDB
 
 app.use(cors());
 app.use(express.json());
 
-
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(() => console.log('MongoDB connecté'))
-    .catch(err => console.log(err));
-
-
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
-
+app.use('/api/mailer', require('./routes/mailerRoutes'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
-    
+app.listen(PORT, () => console.log(`🚀 Serveur démarré sur le port ${PORT}`)); 
