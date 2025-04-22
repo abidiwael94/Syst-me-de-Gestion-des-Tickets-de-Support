5b33715afcea1890cf01f239ff3346301736e699
AnIscoder01 <anisrojo519@gmail.com>
2025-04-22 23:23:52 +0100

    possible conflit

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.set('view engine', 'ejs');
 
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -20,7 +21,14 @@ mongoose.connect(process.env.MONGO_URI, {
 
 app.use('/api/auth', require('./routes/authRoutes'));
 
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
-    
